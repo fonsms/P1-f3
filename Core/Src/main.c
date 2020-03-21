4fe5536c4c497a8c9a8722eed6348618859fc197
fonsms <alfonsomsanz@alumnos.upm.es>
2020-03-21 14:33:35 +0100

    Definitivo y comentado, a la espera de la revisión de mi compañero

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -53,7 +53,7 @@
 #define pulse_per_revolution 3536
 #define max_enconder_count 65535
 #define num_muestras 1200
-#define num_muestras_controlador 3000
+#define num_muestras_controlador 4000
 int32_t count = 0;
 double cuenta2 = 0;
 double cuenta = 0;
@@ -63,9 +63,9 @@ uint32_t i = 0;
 char str_name_reducer[100];
 char str_name[20001];
 double pos_buffer[num_muestras_controlador+1];
-double kp = 10;
-double kd = 1;
-double ki = 0.1;
+double kp = 21;
+double kd = 100;
+double ki = 0.10;
 double referencia = 0;
 double current_value = 0;
 double diff = 0;
@@ -77,14 +77,14 @@ double e_sum =  0;
 //Flags
 bool FLAG_REDUCER = false;
 bool FLAG_TRANSFER = false;
-bool FLAG_PROPORTIONAL_CONTROLER = false;
+bool FLAG_CONTROLLER_SENT_CONTROLER = false;
 bool FLAG_DERIVATIVE_CONTROLER = false;
 bool FLAG_INTEGRATOR_CONTROLER = false;
 bool FLAG_COUNT_OVERFLOW = false;
-bool FLAG_PROPORTIONAL = false;
+bool FLAG_CONTROLLER_SENT = false;
 
 //enum
-enum Controlador{Lineal, Derivativo, Integrador};
+enum Controlador{Proportional, Derivativo, Integrador};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -109,112 +109,121 @@ void funtion_trasfer(double_t V);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-  
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* USER CODE BEGIN Init */
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE END Init */
+	/* USER CODE BEGIN Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN SysInit */
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE END SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
-  MX_TIM6_Init();
-  /* USER CODE BEGIN 2 */
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	MX_TIM2_Init();
+	MX_TIM3_Init();
+	MX_TIM6_Init();
+	/* USER CODE BEGIN 2 */
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
 	HAL_TIM_Encoder_Start_IT(&htim2, TIM_CHANNEL_ALL);
 	HAL_TIM_Base_Start_IT(&htim2);
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 
 	//reductora();// Uncommenting this line to calculate the reducer value
-	//funtion_trasfer(12);// Uncommenting this line to calculate the function transfer
 
-	setref(-3*M_PI,1); // set a first  ref to linear controler
 
+	/*//Uncommenting these lines to obtain the experimental function transfer
+	selec_voltage(0);
+	HAL_Delay(1000);
+	//funtion_trasfer(12);
+	 */
+	/*
+	//Uncommenting these lines to use the controller and rotate the motor to the desired position.
+	selec_voltage(0);
+	HAL_Delay(1000);
+	setref(M_PI,Proportional); // set a first  ref to linear controler
+	 */
 	while (1)
 	{
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 
 
 
 
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL15;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL15;
+	RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_TIM2
-                              |RCC_PERIPHCLK_TIM34;
-  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-  PeriphClkInit.Tim2ClockSelection = RCC_TIM2CLK_HCLK;
-  PeriphClkInit.Tim34ClockSelection = RCC_TIM34CLK_HCLK;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_TIM2
+			|RCC_PERIPHCLK_TIM34;
+	PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+	PeriphClkInit.Tim2ClockSelection = RCC_TIM2CLK_HCLK;
+	PeriphClkInit.Tim34ClockSelection = RCC_TIM34CLK_HCLK;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -234,10 +243,12 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 
 	if (htim->Instance==TIM6) {
-
-		if (FLAG_PROPORTIONAL_CONTROLER == true||FLAG_DERIVATIVE_CONTROLER == true ||FLAG_INTEGRATOR_CONTROLER == true) {
+		//Timer for controller
+		if (FLAG_CONTROLLER_SENT_CONTROLER == true||FLAG_DERIVATIVE_CONTROLER == true ||FLAG_INTEGRATOR_CONTROLER == true) {
 			i += 1;
+			//save the current encoder counter value
 			current_value =__HAL_TIM_GET_COUNTER(&htim2);
+			//If it is counting down we have to subtract the maximum value of the counter to obtain the negative turn
 			if(__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim2)){
 
 				if(FLAG_COUNT_OVERFLOW == true && current_value > last_value ){
@@ -247,33 +258,29 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 					diff = -(last_value - current_value);
 					FLAG_COUNT_OVERFLOW = false;
 				}
-
-
-
 			}
 			else {
-
 				if (FLAG_COUNT_OVERFLOW == true && current_value < last_value) {
 					diff = (max_enconder_count - last_value) + current_value;
 				} else {
 					diff = current_value - last_value;
 					FLAG_COUNT_OVERFLOW = false;
-
 				}
-
 			}
 
 			last_value = current_value;
+			//the current position of the motor is the previous position plus what has been increased
 			pos_i = pos_i + diff;
+			//save the pos_i in a buffer to send later
 			if(i<num_muestras_controlador){
 				pos_buffer[i]= pos_i;
 			}
 
-
-			if(i == num_muestras_controlador && FLAG_PROPORTIONAL == true){
+			if(i == num_muestras_controlador){
 				enviarcuenta();
 			}
-			if (FLAG_PROPORTIONAL_CONTROLER == true) {
+
+			if (FLAG_CONTROLLER_SENT_CONTROLER == true) {
 				controlador_proporcional(pos_i);
 			}
 			if (FLAG_DERIVATIVE_CONTROLER == true) {
@@ -286,8 +293,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 
 
 		}
+		//timer for reducer
 		if (FLAG_REDUCER == true) {
 			i += 1;
+		//wait 4 seconds to turn the engine over and send the account value
 			if(i == 4000){
 				cuenta = __HAL_TIM_GET_COUNTER(&htim2);
 				cuenta2 = TIM2->CNT;
@@ -296,17 +305,15 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 				FLAG_REDUCER = false;
 			}
 		}
+		//timer for function transfer
 		if (FLAG_TRANSFER == true) {
 			medidas[i] = __HAL_TIM_GET_COUNTER(&htim2);
 			i += 1;
 			if(i == 600){
 				selec_voltage(0);
-
-				i += 1;
 			}
 			else if(i == 1200){
 				selec_voltage(0);
-
 				HAL_TIM_Base_Stop_IT(&htim6);
 				enviarcuenta();
 				FLAG_TRANSFER = false;
@@ -336,7 +343,7 @@ void move(double_t v1,double_t v2){
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
 	v1 = round(v1);
 	v2 = round(v2);
-
+//Protection of H bridge
 	if(v1>999){
 		v1 = 999;
 	}
@@ -355,6 +362,7 @@ void move(double_t v1,double_t v2){
 		v1 = 0;
 		v2 = 0;
 	}
+	//Set PWM pin value
 	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1,v1);
 	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2,v2);
 }
@@ -390,7 +398,7 @@ void selec_voltage (double_t V){
  */
 void enviarcuenta(){
 	if (FLAG_TRANSFER == true) {
-		for(int i = 0; i<num_muestras_controlador; i++){
+		for(int i = 0; i<num_muestras; i++){
 			sprintf(str_name, "%s%d\t%f\n",str_name , i,medidas[i]);
 		}
 		sprintf(str_name, "%s#",str_name);
@@ -400,15 +408,15 @@ void enviarcuenta(){
 		sprintf(str_name_reducer, "Cuenta = %f\t %f", cuenta,cuenta2);
 		HAL_UART_Transmit(&huart2,(uint8_t*) str_name_reducer, strlen(str_name_reducer), HAL_MAX_DELAY);
 	}
-	if (FLAG_PROPORTIONAL == true) {
+	if (FLAG_CONTROLLER_SENT == true) {
 		for(int i = 0; i<(int)(num_muestras_controlador/2); i++){
 			sprintf(str_name, "%s%d\t%f\n",str_name , i,pos_buffer[i]);
 		}
 		HAL_UART_Transmit(&huart2,(uint8_t*) str_name, strlen(str_name), HAL_MAX_DELAY);
 		strcpy(str_name,"");
 		for(int i = (num_muestras_controlador/2); i<(int)(num_muestras_controlador); i++){
-					sprintf(str_name, "%s%d\t%f\n",str_name , i,pos_buffer[i]);
-				}
+			sprintf(str_name, "%s%d\t%f\n",str_name , i,pos_buffer[i]);
+		}
 		sprintf(str_name, "%s#",str_name);
 		HAL_UART_Transmit(&huart2,(uint8_t*) str_name, strlen(str_name), HAL_MAX_DELAY);
 	}
@@ -444,15 +452,16 @@ void setref(double ref, enum Controlador controlador){
 	referencia = ref;
 	__HAL_TIM_SET_COUNTER(&htim2, 0);
 	if (controlador == Lineal) {
-		FLAG_PROPORTIONAL_CONTROLER = true;
-		FLAG_PROPORTIONAL = true;
+		FLAG_CONTROLLER_SENT_CONTROLER = true;
+		FLAG_CONTROLLER_SENT = true;
 	}
 	if (controlador == Derivativo) {
 		FLAG_DERIVATIVE_CONTROLER = true;
-
+		FLAG_CONTROLLER_SENT = true;
 	}
 	if (controlador == Integrador) {
 		FLAG_INTEGRATOR_CONTROLER = true;
+		FLAG_CONTROLLER_SENT = true;
 	}
 
 	HAL_TIM_Base_Start_IT(&htim6);
@@ -490,31 +499,31 @@ void funtion_trasfer(double_t V){
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(char *file, uint32_t line)
 { 
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
